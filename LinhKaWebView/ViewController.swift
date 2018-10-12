7837e1c72ab2b474b4308129727e3ab891336a2f
Đoàn Việt <vtahdn@gmail.com>
2018-10-12 14:21:00 +0700

    Initial Commit

---
+++ LinhKaWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinhKaWebView
+//
+//  Created by macbook on 10/12/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
